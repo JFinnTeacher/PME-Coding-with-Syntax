d2d38176afe310310b99599e37dd10dbc5b61f82
Jim Finn <jim.finnjim2001@gmail.com>
2023-12-02 15:57:48 +0000

    Live Classes

--- Code Folder/Workshop01/02 Numbers.py
+++ Code Folder/Workshop01/02 Numbers.py
@@ -1,5 +1,5 @@
 # initial Version
-
+'''
 num = 0
 average = 0
 
@@ -20,3 +20,19 @@ for i in range (numbs):
 print("The total of the", numbs, " numbers entered is ", num)
 average = num/numbs
 print("The Average of these numbers are ", average)
+'''
+# Max and Min
+num = 0
+numList = []
+average = 0
+numbs = int(input("How many numbers do you need to find the average of? "))
+
+for i in range (numbs):
+    tempNum = int(input("Please enter a number "))
+    numList.append(tempNum)
+    num += tempNum
+print("The total of the", numbs, " numbers entered is ", num)
+average = num/numbs
+print("The Average of these numbers are ", average)
+print(max(numList))
+print(min(numList))
