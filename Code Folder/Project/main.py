3a534ebd5b738aeba0914336738f1ed25adbf9b8
Jim Finn <jim.finnjim2001@gmail.com>
2024-01-06 20:59:13 +0000

    Updatesa

--- Code Folder/Project/main.py
+++ Code Folder/Project/main.py
@@ -5,7 +5,13 @@ Date: 2023/01/06
 '''
 # Open and import libraries
 
-
 # initialise lists and variables
+# List = Question, AnswerA, AnswerB, AnswerC, AnswerD, Answer Key
+# Variables: Score, Name, Number of Questions available, Number of questions needed
 
-#Read in File
+# Read in Questions, Answers and Answer Keys and pass to list (Function)
+# Identify how many questions in list
+
+# Start Game
+# Ask for Player name
+# Ask for how many Questions for quiz
