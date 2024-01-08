8e0a97d6a55caa09b2abd3229b6e187ea17d5b1f
Jim Finn <jim.finnjim2001@gmail.com>
2024-01-08 20:57:27 +0000

    Update

--- Code Folder/Project/main.py
+++ Code Folder/Project/main.py
@@ -2,5 +2,6 @@
 Title: Electronics Calculator
 Name: Jim Finn
 Date: 2024/01/07
-Requirements: Variables and Operators, Sequence of Statements, Conditional Statements, Iteration, Data Types & Structures, Functions
+Requirements:   Variables and Operators, Sequence of Statements, Conditional Statements, 
+                Iteration, Data Types & Structures, Functions
 '''
