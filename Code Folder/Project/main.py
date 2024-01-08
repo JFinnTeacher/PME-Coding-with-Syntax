827ef47f064843e9d2cf0d88a5468ea2138a154e
Jim Finn <jim.finnjim2001@gmail.com>
2024-01-08 08:00:16 +0000

    Initial Work on Quiz

--- Code Folder/Project/main.py
+++ Code Folder/Project/main.py
@@ -1,17 +1,51 @@
 '''
-Title: PME Project - Multiple Choice Quiz
+Title: Coding with Syntax Project - Multiple Choice Quiz
 Name: Jim Finn
 Date: 2023/01/06
+Requirements: Variables and Operators, Sequence of Statements, Conditional Statements, Iteration, Data Types & Structures, Functions
 '''
 # Open and import libraries
+# Libraries are csv for working with question files, random, os for listing available quizes
+import csv
+import random
+import os
 
 # initialise lists and variables
 # List = Question, AnswerA, AnswerB, AnswerC, AnswerD, Answer Key
-# Variables: Score, Name, Number of Questions available, Number of questions needed
+# Variables: Score, Name, Number of questions needed
+question = []
+score = 0
+name = " "
+noQuestions = 0
 
+# List all files in Question Database Folder
+# Ask User for a quiz by either typing in Quiz name or choosing number (Preferred)
 # Read in Questions, Answers and Answer Keys and pass to list (Function)
-# Identify how many questions in list
+absolutePath = os.path.dirname(__file__) # Solution found @ https://towardsthecloud.com/get-relative-path-python
+pathQuestions = "questions"
+fullPath = os.path.join(absolutePath, pathQuestions)
+os.listdir(fullPath)
+
+# Ask for Player name
+# Give users a choice between 5, 10, 15 or 20 Questions
+# If outside this range display error and ask again.
 
 # Start Game
-# Ask for Player name
-# Ask for how many Questions for quiz
+# Welcome message and instructions
+
+# pick a question at random from the list loaded and display question with all 4 options
+
+
+# Ask for user input by choosing A to D
+
+
+# check answer against answer key
+# if not an accepted answer A to D ask again (Error Message)
+    # If correct add 10 to score
+    # if Incorrect subtract 5 from score
+
+# Check if you have asked all questions
+# if yes then end quiz otherwise ask another one
+
+# end quiz (Function)
+# display final score and proportion of right and wrong questions
